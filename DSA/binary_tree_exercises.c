f87f808728d9cb696059c329cacf62a83079cd1a
Sam Meredith <sam.meredith@npqc.com>
2020-06-02 13:11:03 +0100

    Remaining binary tree exercises.

--- DSA/binary_tree_exercises.c
+++ DSA/binary_tree_exercises.c
@@ -31,6 +31,25 @@ int has_path_sum(node_t* node, int sum);
 void print_paths(node_t* node);
 void print_paths_recur(node_t* node, int* path, int path_length);
 
+// Ex 9.
+void mirror(node_t* node);
+
+// Ex 10.
+void double_tree(node_t* node);
+
+// Ex 11.
+bool same_tree(node_t* a, node_t* b);
+
+// Ex 12.
+int count_trees(int num_keys);
+
+// Ex 13.
+bool is_BST(node_t* node);
+
+// Ex 14.
+bool is_BST2(node_t* node);
+bool is_BST_recur(node_t* node, int min, int max);
+
 int main()
 {
     node_t* root = build123();
@@ -55,6 +74,37 @@ int main()
     
     printf("Tree build123() has tree->leaf paths:\r\n");
     print_paths(root);
+    
+    mirror(root);
+    printf("The postorder traversal of mirror(build123()) is ");
+    print_postorder(root);
+    printf("\r\n");
+    
+    double_tree(root);
+    printf("The postorder traversal of double_tree(mirror(build123())) is ");
+    print_postorder(root);
+    printf("\r\n");
+    
+    node_t* test  = build123();
+    mirror(test);
+    printf("Is mirror(build123()) the same as our working tree? %d\r\n",
+        same_tree(root, test));
+    double_tree(test);
+    printf("Is double_tree(mirror(build123())) the same as our working tree? %d\r\n",
+        same_tree(root, test));
+        
+    printf("count_trees(4) is %d\r\n", count_trees(4));
+    printf("count_trees(5) is %d\r\n", count_trees(5));
+    
+    printf("Is double_tree(mirror(build123())) a BST? %d\r\n", is_BST(root));
+    test = build123();
+    double_tree(test);
+    printf("Is double_tree(build123()) a BST? %d\r\n", is_BST(test));
+    
+    printf("Is double_tree(mirror(build123())) a BST2? %d\r\n", is_BST(root));
+    test = build123();
+    double_tree(test);
+    printf("Is double_tree(build123()) a BST2? %d\r\n", is_BST(test));
     
     return 0;
 }
@@ -167,7 +217,7 @@ int has_path_sum(node_t* node, int sum)
     if (node->right != NULL)
         has_right_path_sum = has_path_sum(node->right, sum - node->key);
     
-    return has_left_path_sum | has_right_path_sum;
+    return has_left_path_sum || has_right_path_sum;
 }
 
 //-------------------------------------------
@@ -209,4 +259,115 @@ void print_paths_recur(node_t* node, int* path, int path_length)
         print_paths_recur(node->left, path, path_length);
         print_paths_recur(node->right, path, path_length);
     }
+}
+
+//-------------------------------------------
+// Mirror the given tree
+//-------------------------------------------
+void mirror(node_t* node)
+{
+    if (node == NULL)
+        return;
+    
+    node_t* temp = node->right;
+    node->right = node->left;
+    node->left = temp;
+    
+    mirror(node->right);
+    mirror(node->left);
+}
+
+//-------------------------------------------
+// For each node in a binary search tree, create a new duplicate node as the left child of the original node
+//-------------------------------------------
+void double_tree(node_t* node)
+{
+    if (node == NULL)
+        return;
+    
+    node_t *new_node = (node_t*)malloc(sizeof(node_t));
+    new_node->key = node->key;
+    new_node->left = node->left;
+    new_node->right = NULL;
+    
+    node->left = new_node;
+    
+    double_tree(node->right);
+    double_tree(new_node->left);
+}
+
+//-------------------------------------------
+// Return true if the trees are structurally identical
+//-------------------------------------------
+bool same_tree(node_t* a, node_t* b)
+{
+    if (a == NULL && b == NULL)
+        return true;
+    
+    if (a == NULL || b == NULL)
+        return false;
+    
+    return (a->key == b->key && same_tree(a->left, b->left) && same_tree(a->right, b->right));
+}
+
+//-------------------------------------------
+// Returns the number of structurally unique binary search trees with keys 1,...,num_keys
+//-------------------------------------------
+int count_trees(int num_keys)
+{
+    int count = 0;
+    
+    if (num_keys <= 1)
+        return 1;
+    
+    for (int i = 1; i <= num_keys; i++)
+    {
+        count += count_trees(i - 1) * count_trees(num_keys - i);
+    }
+    
+    return count;
+}
+
+//-------------------------------------------
+// Returns true if a tree is a BST
+//-------------------------------------------
+bool is_BST(node_t* node)
+{
+    if (node == NULL)
+        return true;
+    
+    // Tree is a BST if, for each node, all nodes left are <= and all nodes right are >
+    bool is_left_BST;
+    if (node->left != NULL && max_value(node->left) > node->key)
+        is_left_BST = false;
+    else is_left_BST = is_BST(node->left);
+    
+    bool is_right_BST;
+    if (node->right != NULL && min_value(node->right) <= node->key)
+        is_right_BST = false;
+    else is_right_BST = is_BST(node->right);
+    
+    return is_left_BST && is_right_BST;
+}
+
+//-------------------------------------------
+// Returns true if a tree is a BST
+//-------------------------------------------
+bool is_BST2(node_t* node)
+{
+    return is_BST_recur(node, INT_MIN, INT_MAX);
+}
+
+//-------------------------------------------
+// 
+//-------------------------------------------
+bool is_BST_recur(node_t* node, int min, int max)
+{
+    if (node == NULL);
+        return true;
+    
+    if (node->key > max || node->key < min)
+        return false;
+    
+    return is_BST_recur(node->left, min, node->key) && is_BST_recur(node->right, node->key + 1, max);
 }
